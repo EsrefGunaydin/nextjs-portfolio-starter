27f7a4b25ae532711a8102c6c391e624ed1eee30
Esref Gunaydin <esrefgunaydin@gmail.com>
2021-06-22 15:43:47 -0700

    test test

--- pages/posts/test.jsx
+++ pages/posts/test.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Head from "next/head"
 
 const test = () => {
     return (
         <div>
+        <Head>
+          <title>This is the test page</title>
+        </Head>
             <h1>This is a test page</h1>
         </div>
     )
