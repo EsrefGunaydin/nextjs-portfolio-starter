6aec0ddda2660d9e269532d3e82749bb66e68224
Esref Gunaydin <esrefgunaydin@gmail.com>
2021-06-22 15:50:28 -0700

    ..

--- pages/posts/test.jsx
+++ pages/posts/test.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
-import Head from "next/head"
+import Head from 'next/head'
 
 const test = () => {
-    return (
-        <div>
-        <Head>
-          <title>This is the test page</title>
-        </Head>
-            <h1>This is a test page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Head>
+        <title>Test</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>This is a test page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum at debitis
+        odit non dolorum assumenda doloribus nobis! At molestiae itaque cumque
+        excepturi minus? Temporibus aliquid, quos minus possimus nobis sint!
+      </p>
+    </div>
+  )
 }
 
 export default test
